a2dc21154caa7eb640805c9f9ad1cf8522d7d960
GeovanniVeloso <141738879+GeovanniVeloso@users.noreply.github.com>
2023-08-14 20:22:56 -0300

    Add files via upload

--- src/controller/RecursivaSoma.java
+++ src/controller/RecursivaSoma.java
@@ -4,7 +4,7 @@ public class RecursivaSoma {
 	public RecursivaSoma() {
 		super();
 	}
-	
+	//Explicação: Utiliza a variável n2 como um contador, definindo sua parada para quando n2 for igual a 1, retornando n1 quando atingido ( pois qualquer número vezes 1 é o próprio número ).
 	public int soma (int n1, int n2, int soma) {
 		if (n2 == 1) {
 			return n1;
